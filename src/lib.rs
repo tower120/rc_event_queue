02b972c337048d7bc64763c74e928f6cfe6dec0f
Andy <tower120@gmail.com>
2021-08-29 14:38:52 +0300

    initial bench

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,16 @@ mod EventReader;
 mod chunk;
 mod event;
 
+
+#[inline]
+pub fn fibonacci(n: u64) -> u64 {
+    match n {
+        0 => 1,
+        1 => 1,
+        n => fibonacci(n-1) + fibonacci(n-2),
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
